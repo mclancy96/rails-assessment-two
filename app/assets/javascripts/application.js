7d1c04862b7bca6fbd08123634539b4ba469e350
Mike <mclancy96@gmail.com>
2025-08-25 08:30:11 -0400

    Upgrade to rails 7.1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,9 +7,5 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file. JavaScript code in this file should be added after the last require_* statement.
 //
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require rails-ujs
-//= require turbolinks
-//= require_tree .
+
+// Sprockets and Turbolinks are not included by default in Rails 7.1. Remove directives if not using them.
