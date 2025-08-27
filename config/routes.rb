cbaba26681410fa68430fb43e519a4b40f008dc1
Mike <mclancy96@gmail.com>
2025-08-27 10:48:25 -0400

    Display sweet form and button

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :sweets, only: %i[index show]
   resources :vendors, only: %i[index show]
+  resources :vendor_sweets, only: %i[create destroy]
 end
